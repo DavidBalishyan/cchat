dbcaf6cd83d85f19406bd5cdc58a19e47d317baf
DavidBalishyan <davidbalishyan12@gmail.com>
2026-08-18 23:21:31 +0400

    Accidentally had **/server in .gitignore

---
+++ server/loader.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+
+#define PORT 8080
+#define MAX_CLIENTS 10
+#define BUFFER_SIZE 1024
+
